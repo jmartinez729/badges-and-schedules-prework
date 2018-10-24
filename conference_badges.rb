2480b5f79d724e7f808e2110c01ac17d6fc2316f
John Martinez <jmartinez729@gmail.com>
2018-10-24 17:43:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,8 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator(array_of_names)
-  array_of_names.each do |name|
+def batch_badge_creator(badges)
+  badges.each do |name|
     return "Hello, my name is #{name}."
   end 
   
