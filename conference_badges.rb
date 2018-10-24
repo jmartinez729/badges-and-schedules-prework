d00615bbc6d98ddf679af1a39508948bb6930817
John Martinez <jmartinez729@gmail.com>
2018-10-24 17:52:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,10 +10,19 @@ def batch_badge_creator(badges)
   end
 end 
 
-def assign_rooms(badges)
+def assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
 
-  badges.each_with_index do |name, index|
-    return "Hello, #{name}! You'll be assigned to room #{index + 1}!"
-    
-  end 
-end 
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
